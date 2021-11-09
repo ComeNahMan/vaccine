a323b6ceb04d10601a547c1540aee0112d80ebca
ComeNahMan <xtremeeligon@gmail.com>
2021-11-09 13:23:12 -0400

    added how many days till fully vaccinated

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 var status = true;
 var name = "Matt";
-var prompt = "Welcome "+ name + " Thank You for being fully vaccinated you can now enter!"
+var prompt = "Welcome "+ name + " You're vaccine card has been recieved!"
 
 var el = document.getElementById('end');
 
 el.textContent = prompt;
+
+var vaxCard = {
+  name: "Lasko Fan",
+  vaccineType: "Sinopharm",
+  sex: "M",
+  age: "25",
+  fullyVaccined: true,
+  secondShot: new Date('nov 6, 2021 12:30:00'),
+    findDifference: function(){
+      var today = new Date()
+      var difference = today.getTime() - vaxCard.secondShot.getTime();
+      difference = Math.floor(difference/84600000);
+      return difference;
+    }
+};
+
+var date = vaxCard.findDifference();
+var daysUntil = 14 - date;
+
+
+if(vaxCard.fullyVaccined == true) {
+  if(date >=14) {
+    var message = document.getElementById("message");
+   message.textContent = "You're now Fully Vaccinated!";
+  } else {
+    var message = document.getElementById("message");
+    message.textContent = "There's " + daysUntil + " days left for you to be fully vaccinated.";
+  }
+} else { var message = document.getElementById("message");
+    message.textContent = "You're not fully vaccinated yet gone from here :).";
+ }
