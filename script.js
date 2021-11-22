fb0109b7e4fc2bedfb69a1ace65d1dd06e1de8e8
ComeNahMan <xtremeeligon@gmail.com>
2021-11-22 14:17:15 -0400

    added in summary report

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var status = true;
+/*var status = true;
 var name = "Matt";
 var prompt = "Welcome "+ name + " You're vaccine card has been recieved!"
 
@@ -35,4 +35,123 @@ if(vaxCard.fullyVaccined == true) {
   }
 } else { var message = document.getElementById("message");
     message.textContent = "You're not fully vaccinated yet gone from here :).";
+ }
+ */
+
+
+ function idCard(personName, expiration) {
+   this.name = personName;
+   this.expiration = new Date(expiration);
+   this.today = new Date();
+   this.checkExpiration = function() { //method
+     var difference = this.today.getTime() - this.expiration.getTime();
+     var sign = Math.sign(difference)
+     if (sign == -1) {
+       return false;
+     } else if (sign == 1) {
+       return true;
+     }
+   }
+   this.expiration = this.checkExpiration();
+ }
+
+ var person1 = new idCard('Freddy Kruger', '2022-04-01');
+ var person2 = new idCard('Jack Skellington', '2023-10-05');
+ var person3 = new idCard('Santa Claus', '2020-12-25');
+ var person4 = new idCard('Lasko Fan', '2024-05-09');
+
+
+
+ var idCardArray = [person1, person2, person3, person4];
+
+ function vaxCard(personName, date, vaccined) {
+   this.name = personName;
+   this.secondShot = new Date(date);
+   this.vaccined = vaccined;
+   this.getDifference = function() {
+     var today = new Date();
+     var difference = today.getTime() - this.secondShot.getTime();
+     difference = Math.floor(difference / 86400000);
+     return difference;
+   }
+
+
+   this.checkVaccine = function(index) {
+     var date = this.getDifference();
+
+     var daysUntil = 14 - date;
+
+     if (daysUntil == 1) {
+       daysMessage = "one more day";
+     } else {
+       daysMessage = " days";
+     }
+
+     if (idCardArray[index].expiration == false) {
+       if (idCardArray[index].name == this.name) {
+         if (this.vaccined == true) {
+           if (date >= 14) {
+             var message = " You're Fully Vaccinated! ";
+             return message;
+           } else {
+             var message = daysUntil + daysMessage + " till you could enter. ";
+             return message;
+           }
+         } else {
+           var message = " You're Not Fully Vaccinated.";
+           return message;
+         }
+       } else {
+         var message = " Names on both ID and Vaccine card do not match. ";
+         return message;
+       }
+     } else {
+       var message = " Your ID is Expired. ";
+       return message;
+     }
+   };
+
+   this.access = function(index) {
+     var date = this.getDifference();
+
+     var daysUntil = 14 - date;
+
+     if (daysUntil == 1) {
+       daysMessage = "one more day";
+     } else {
+       daysMessage = " days";
+     }
+
+     if (idCardArray[index].expiration == false) {
+       if (idCardArray[index].name == this.name) {
+         if (this.vaccined == true) {
+           if (date >= 14) {
+             return "You Can Come In ";
+           } else {
+             return "Nope";
+           }
+         } else {
+           return "Gone From Here";
+         }
+       } else {
+         return "Rock So";
+       }
+     } else {
+       return "Hardlucks";
+     }
+   };
+ }
+
+ var personVax1 = new vaxCard('Freddy Krueger', '2021-10-01', true);
+ var personVax2 = new vaxCard('Jack Skellington', '2021-10-01', true);
+ var personVax3 = new vaxCard('Santa Claus', undefined, false);
+ var personVax4 = new vaxCard('Lasko Fan', '2021-10-26', true);
+
+
+ var vaxCardArray = [personVax1, personVax2, personVax3, personVax4];
+
+ var vaxArrayLength = vaxCardArray.length;
+ for (var i = 0; i < vaxArrayLength; i++) {
+   document.write(idCardArray[i].name,)
+   document.write(vaxCardArray[i].checkVaccine(i))
  }
